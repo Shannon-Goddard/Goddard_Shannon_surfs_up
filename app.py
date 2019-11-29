cea9e8c27adfd8a39a9af9b45f27a3af0258d27e
unknown <Uthingkithunk@aol.com>
2019-11-29 11:30:53 -0800

    added to code

--- app.py
+++ app.py
@@ -33,3 +33,42 @@ def welcome():
         f"/api/v1.0/tobs<br/>"
         f"/api/v1.0/temp/start/end<br/>"
 	)
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+        prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+        precipitation = session.query(measurement.date, measurement.prcp).\
+		filter(measurement.date >= prev_year).all()
+        precip = {date: prcp for date, prcp in precipitation}
+        return jsonify(precip)
+@app.route("/api/v1.0/stations")
+def stations():
+        results = session.query(station.station).all()
+        stations = list(np.ravel(results))
+        return jsonify(stations)
+@app.route("/api/v1.0/tobs")
+def temp_monthly():
+        prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
+        results = session.query(measurement.tobs).\
+                filter(measurement.station == 'USC00519281').\
+                filter(measurement.date >= prev_year).all()
+
+        temps = list(np.ravel(results))
+        return jsonify(temps)  
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+def stats():
+
+        sel = [func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)]
+
+        if not End:
+                results = session.query(*sel).\
+                        filter(measurement.date >= Start).\
+                        filter(measurement.date <= End).all()
+                temps = list(np.ravel(results))
+        return jsonify(temps)
+        results = session.query(*sel).\
+                        filter(measurement.date >= Start).\
+                        filter(measurement.date <= End).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps)
